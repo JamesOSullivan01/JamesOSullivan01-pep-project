2c2e6b22cc7f9a9903f6a2ae1bcb4a31831fe26a
James O'Sullivan <james.a.osullivan01@gmail.com>
2024-02-16 22:25:47 +0000

    Started writing registernewUser method.

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,33 @@
+package ;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAO {
+        public Account registerNewUser(Account account){
+                Connection connection = ConnectionUtil.getConnection();
+                try{
+                    String SQL = "INSERT INTO account (username, password) VALUES (?,?)";
+                    PreparedStatement ps = connection.prepareStatement(SQL,  java.sql.Statement.RETURN_GENERATED_KEYS);
+                    ps.setString(1, account.getUsername());
+                    ps.setString(2, account.getPassword());
+                    ps.executeUpdate();
+
+                    ResultSet generatedKeys = ps.getGeneratedKeys();
+                    if (generatedKeys.next()) {
+                    int generatedId = generatedKeys.getInt(1);
+                    account.setAccount_id(generatedId);
+                    return new Account(generatedId, account.getUsername(), account.getPassword());
+                }
+
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+                    return null;
+        }
+}
